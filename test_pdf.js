cc872eec92e13c3beb6818d46b19b41f95b458f1
joelalbino19 <joelalbino19@gmail.com>
2026-05-09 09:50:45 -0500

    volantes

--- test_pdf.js
+++ test_pdf.js
@@ -2,18 +2,17 @@ const fs = require('fs');
 const pdfjsLib = require('pdfjs-dist/legacy/build/pdf.js');
 
 async function extractText() {
-    const dataBuffer = fs.readFileSync('TIRA DE PAGO BASICO ARTDECON ABRIL 2026.pdf');
+    const dataBuffer = fs.readFileSync('TIRA DE PAGO BASICO TOP TALENT ABRIL 2026.pdf');
     const loadingTask = pdfjsLib.getDocument({ data: new Uint8Array(dataBuffer) });
     const pdf = await loadingTask.promise;
     let fullText = "";
 
-    // Just print the first page for analysis
     for (let i = 1; i <= 1; i++) {
         const page = await pdf.getPage(i);
         const textContent = await page.getTextContent();
         const pageText = textContent.items.map(item => item.str).join('\n');
         fullText += pageText + "\n";
     }
-    console.log(fullText);
+    console.log(fullText.substring(0, 1000));
 }
 extractText().catch(console.error);
